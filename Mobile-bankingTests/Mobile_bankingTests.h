a912c3db7e71d675a33756a6852766e143867cf1
dendoronin <ddoronin@itgroups.com.ua>
2013-01-04 11:20:03 +0200

    Initial Commit

---
+++ Mobile-bankingTests/Mobile_bankingTests.h
@@ -0,0 +1,13 @@
+//
+//  Mobile_bankingTests.h
+//  Mobile-bankingTests
+//
+//  Created by   Doro on 1/4/13.
+//  Copyright (c) 2013 Doro. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Mobile_bankingTests : SenTestCase
+
+@end
---
+++ Mobile-bankingTests/Mobile_bankingTests.m
@@ -0,0 +1,32 @@
+//
+//  Mobile_bankingTests.m
+//  Mobile-bankingTests
+//
+//  Created by   Doro on 1/4/13.
+//  Copyright (c) 2013 Doro. All rights reserved.
+//
+
+#import "Mobile_bankingTests.h"
+
+@implementation Mobile_bankingTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Mobile-bankingTests");
+}
+
+@end
